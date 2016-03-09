56f83e39e796f56cceab00a05e06bd27abd61b45
Matthew Keller <mkeller2019@pingry.org>
2016-03-09 09:30:42 -0500

    takes user input for a base and exponenet and sets the base to the exponenent

---
+++ powerFunction.py
@@ -0,0 +1,11 @@
+def power_base(base, exponent):
+	count = 1
+	number = base 
+	while count < exponent:
+		number *= base
+		count += 1
+	return number
+	
+base = int(raw_input("What would you like the base to be? "))
+exponent = int(raw_input("What would you like the exponenet to be? "))
+print(power_base(base, exponent))
